d2fc91b71292900fe5af3a804df41fb3aa1d5be3
Myeonghak Lee <mhmh779@naver.com>
2022-11-01 11:08:06 +0900

    base

--- pjt/settings.py
+++ pjt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,6 +123,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join("static"),)
 
 MEDIA_ROOT = BASE_DIR / "images"
 MEDIA_URL = "/media/"
