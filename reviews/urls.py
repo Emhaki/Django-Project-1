f93068fd5892c6470259580242d2bba57534ddb3
Myeonghak Lee <mhmh779@naver.com>
2022-11-01 17:27:16 +0900

    AddStore

--- reviews/urls.py
+++ reviews/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path("store/", views.store, name="store"),
     path("<int:store_pk>/", views.store_detail, name="store_detail"),
     path("<int:store_pk>/review_create/", views.review_create, name="review_create"),
-    path("<int:review_pk>/review/", views.review_detail, name="review_detail"),
 ]
