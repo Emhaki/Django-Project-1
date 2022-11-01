7c05fa993df784c05ce2752890e82d219bb61f87
an dong woo <akskwha@naver.com>
2022-11-01 11:15:11 +0900

    회원가입

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 # Create your views here.
+
+def create(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request,user)
+            return redirect('accounts:create')
+    else:
+        form = CustomUserCreationForm()
+    context={
+        'form':form
+    }
+    return render(request, 'accounts/create.html', context)
