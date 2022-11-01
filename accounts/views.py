ab21672b31211296dc86d0ed10f9e137441a4b44
sony-jun <warrise@naver.com>
2022-11-01 14:20:31 +0900

    accounts,login,follow

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,53 @@
-from django.shortcuts import render,redirect
-from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+
 # Create your views here.
+
 
 def create(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            auth_login(request,user)
-            return redirect('accounts:create')
+            auth_login(request, user)
+            return redirect("accounts:login")
     else:
         form = CustomUserCreationForm()
-    context={
-        'form':form
-    }
-    return render(request, 'accounts/create.html', context)
+    context = {"form": form}
+    return render(request, "accounts/create.html", context)
 
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get("next") or "reviews:index")
+    else:
+        form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("reviews:index")
+
+
+@login_required
+def follow(request, pk):
+    user = get_object_or_404(get_user_model(), pk=pk)
+    if request.user == user:
+        messages.warning(request, "자추금지!!!")
+        return redirect("accounts:detail", pk)
+    if request.user in user.followers.all():
+        user.followers.remove(request.user)
+    else:
+        user.followers.add(request.user)
+    return redirect("accounts:detail", pk)
