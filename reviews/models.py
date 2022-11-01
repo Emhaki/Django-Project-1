8f1297ebbd291330fb56b9669764dd2fae85fe55
JC <chohy1106@gmail.com>
2022-11-01 11:10:36 +0900

    make reviews index, model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
+class Store(models.Model):
+    store_name = models.CharField(max_length=80)
+    address = models.CharField(max_length=160)
+    phone_num = models.CharField(max_length=80)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    menu = models.ForeignKey("Menu", on_delete=models.CASCADE)
+
+
+class Menu(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    menu_name = models.CharField(max_length=80)
+    menu_price = models.IntegerField()
+
+
+class Store_image(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    image = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(1200, 960)],
+        format="JPEG",
+        options={"quality": 90},
+    )
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Store
 
 # Create your views here.
+def index(request):
+    stores = Store.objects.all()
+    context = {
+        "stores": stores,
+    }
+    return render(request, "reviews/index.html", context)
+
+
+def store_detail(request, store_pk):
+    store = Store.objects.get(pk=store_pk)
+    context = {
+        "store": store,
+    }
+    return render(request, "reviews/store_detail.html", context)
