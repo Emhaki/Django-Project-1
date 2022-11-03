c854ee3f3ca5ee7b9783d232dfd592abd83c2a35
sony-jun <warrise@naver.com>
2022-11-03 11:32:49 +0900

    failed

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from reviews.forms import ReviewForm, CommentForm
 from django.shortcuts import redirect, render
 from .forms import StoreForm, CommentForm
 from .models import Store, Review, Comment
 from django.contrib import messages
-
+from django.http import JsonResponse, HttpResponse
 from django.db.models import Avg
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -37,18 +38,18 @@ def store_detail(request, store_pk):
     store = Store.objects.get(pk=store_pk)
     reviews = store.review_set.all()
 
-    if request.POST.get('grade-5'):
-      reviews = Review.objects.filter(grade=5)
-    elif request.POST.get('grade-4'):
-      reviews = Review.objects.filter(grade=4)
-    elif request.POST.get('grade-3'):
-      reviews = Review.objects.filter(grade=3)
-    elif request.POST.get('grade-2'):
-      reviews = Review.objects.filter(grade=2)
-    elif request.POST.get('grade-1'):
-      reviews = Review.objects.filter(grade=1)
-    elif request.POST.get('reset'):
-      reviews = Review.objects.order_by("-pk")
+    if request.POST.get("grade-5"):
+        reviews = Review.objects.filter(grade=5)
+    elif request.POST.get("grade-4"):
+        reviews = Review.objects.filter(grade=4)
+    elif request.POST.get("grade-3"):
+        reviews = Review.objects.filter(grade=3)
+    elif request.POST.get("grade-2"):
+        reviews = Review.objects.filter(grade=2)
+    elif request.POST.get("grade-1"):
+        reviews = Review.objects.filter(grade=1)
+    elif request.POST.get("reset"):
+        reviews = Review.objects.order_by("-pk")
 
     review_5 = Review.objects.filter(grade=5).count()
     review_4 = Review.objects.filter(grade=4).count()
@@ -59,13 +60,13 @@ def store_detail(request, store_pk):
     review_ave = 0
 
     if review_ave == 0:
-      review_ave = "평가 없음"
-    
-    if reviews == True:
-      ave = Review.objects.aggregate(Avg('grade'))
+        review_ave = "평가 없음"
 
-      # round(값, 표시하고 싶은 자리수)
-      review_ave = round(ave['grade__avg'], 2)
+    if reviews == True:
+        ave = Review.objects.aggregate(Avg("grade"))
+
+        # round(값, 표시하고 싶은 자리수)
+        review_ave = round(ave["grade__avg"], 2)
 
     context = {
         "store": store,
@@ -139,14 +140,16 @@ def review_update(request, store_pk, review_pk):
         messages.warning(request, "작성자만 수정할 수 있습니다.")
         return redirect("articles:detail", review.pk)
 
+
 def search(request):
-    search= Store.objects.all().order_by('-pk')
-    q = request.POST.get('q',"")
+    search = Store.objects.all().order_by("-pk")
+    q = request.POST.get("q", "")
     if q:
         search = search.filter(store_name__icontains=q)
-        return render(request, 'reviews/search.html',{'search':search, 'q':q})
+        return render(request, "reviews/search.html", {"search": search, "q": q})
     else:
-        return render(request, 'reviews/search.html')
+        return render(request, "reviews/search.html")
+
 
 def comment_create(request, store_pk, review_pk):
     review = Review.objects.get(pk=review_pk)
@@ -161,6 +164,29 @@ def comment_create(request, store_pk, review_pk):
     return redirect("reviews:review_detail", store_pk, review_pk)
 
 
+"""
+ def comment_create(request, store_pk, review_pk):
+    review = get_object_or_404(Review, pk=review_pk)
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.review = review
+                comment.user = request.user
+                comment.save()
+                context = {
+                    "content": comment.content,
+                    "userName": comment.user.username,
+                }
+                return JsonResponse(context)
+        else:
+            return HttpResponse(status=403)
+    else:
+        return redirect("accounts:login")
+"""
+
+
 def comment_delete(request, store_pk, review_pk, comment_pk):
     comment = Comment.objects.get(pk=comment_pk)
     if request.user == comment.user:
@@ -168,4 +194,3 @@ def comment_delete(request, store_pk, review_pk, comment_pk):
             comment.delete()
             return redirect("reviews:review_detail", store_pk, review_pk)
     return redirect("reviews:review_detail", store_pk, review_pk)
-
